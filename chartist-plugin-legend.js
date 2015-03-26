a4226b85e747d38f8058d62a137dfa3061a80b1b
Kees Kluskens <kees@webduck.nl>
2015-03-26 12:59:52 +0100

    Fix formatting

--- chartist-plugin-legend.js
+++ chartist-plugin-legend.js
@@ -12,6 +12,19 @@
   } else {
     root['Chartist.plugins.legend'] = factory();
   }
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], function () {
+            return (root.returnExportsGlobal = factory());
+        });
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        root['Chartist.plugins.legend'] = factory();
+    }
 }(this, function () {
 
     /**
